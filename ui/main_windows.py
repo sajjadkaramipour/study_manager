ee22a27913246abc76f27c2e4cf0ff3eec3fc274
Sajjad Karami <sajjad.karami2011@gmail.com>
2026-09-08 22:53:22 +0330

    Add initial sidebar
    Add the initial sidebar structure and configure its layout using Tkinter.

--- ui/main_windows.py
+++ ui/main_windows.py
@@ -15,3 +15,24 @@ class Mainwindows:
                 # self.tk.iconbitmap("..\\Icon_Image\\study-manager-icon.ico")
 #---------End windows--------
 
+#---------Start sidebar--------
+                sidebar = Frame(self.tk, bg="indigo", width=240)
+                sidebar.grid(row=0, column=0, sticky="nsew")
+                sidebar.grid_propagate(False)
+                sidebar.grid_columnconfigure(0, weight=1)
+                sidebar.grid_rowconfigure(0, weight=1)
+
+                subjects_button=Button(sidebar, text="Subjects", bg="indigo", font=("Segoe UI",16)
+                , fg="white", bd=0, highlightthickness = 0, pady=5)
+                subjects_button.grid(row=0, column=0, sticky="new")
+
+                timer_button=Button(sidebar, text="Timer", bg="indigo", font=("Segoe UI",16)
+                , fg="white", bd=0,highlightthickness = 0, pady=5)
+                timer_button.grid(row=1, column=0, sticky="new")
+
+                settings_button=Button(sidebar, text="Settings", bg="indigo", font=("Segoe UI",16)
+                , fg="white", bd=0, highlightthickness = 0, pady=5)
+                settings_button.grid(row=2, column=0, sticky="new")
+
+#---------End sidebar---------
+
